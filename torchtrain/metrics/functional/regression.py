6eea8f3b111a3cc13c16cfd6ab9d7de22b374630
vyzyv <vyz@protonmail.com>
2020-07-24 18:52:49 +0200

    Add regression metrics

--- torchtrain/metrics/functional/regression.py
+++ torchtrain/metrics/functional/regression.py
@@ -3,9 +3,26 @@ import torch
 from .. import _base
 
 
+def total_of_squares(target, reduction=torch.sum):
+    return reduction(target - torch.mean(target)) ** 2
+
+
+def regression_of_squares(output, target, reduction=torch.sum):
+    return reduction(output - torch.mean(target)) ** 2
+
+
+def squares_of_residuals(output, target, reduction=torch.sum):
+    return reduction(output - target) ** 2
+
+
 # TBD
-def r2_score():
-    pass
+def r2(output, target):
+    return 1 - squares_of_residuals(output, target) / total_of_squares(target)
+
+
+def adjusted_r2(output, target, p):
+    numel = output.numel()
+    return 1 - (1 - r2(output, target)) * ((numel - 1) / (numel - p - 1))
 
 
 def absolute_error(output, target, reduction=torch.mean):
--- torchtrain/metrics/regression.py
+++ torchtrain/metrics/regression.py
@@ -1,22 +1,53 @@
+import torch
+
 from .. import _base
 from . import functional
 
 
-class _ReductionOnly(_base.Op):
+class _MeanReduction(_base.Op):
     def __init__(self, reduction=torch.mean):
         self.reduction = reduction
 
 
-class R2Score(_base.Op):
-    pass
+class _SumReduction(_base.Op):
+    def __init__(self, reduction=torch.sum):
+        self.reduction = reduction
 
 
-class AbsoluteError(_ReductionOnly):
+class TotalOfSquares(_SumReduction):
     def forward(self, data):
-        return functional.regression.absolue_error(*data, self.reduction)
+        return functional.regression.regression_of_squares(data, self.reduction)
 
 
-class SquaredError(_ReductionOnly):
+class RegressionOfSquares(_SumReduction):
+    def forward(self, data):
+        return functional.regression.regression_of_squares(*data, self.reduction)
+
+
+class SquaresOfResiduals(_SumReduction):
+    def forward(self, data):
+        return functional.regression.squares_of_residuals(*data, self.reduction)
+
+
+class R2(_base.Op):
+    def forward(self, data):
+        return functional.regression.r2(*data)
+
+
+class AdjustedR2(_base.Op):
+    def __init__(self, p: int):
+        self.p = p
+
+    def forward(self, data):
+        return functional.regression.adjusted_r2(*data, self.p)
+
+
+class AbsoluteError(_MeanReduction):
+    def forward(self, data):
+        return functional.regression.absolute_error(*data, self.reduction)
+
+
+class SquaredError(_MeanReduction):
     def forward(self, data):
         return functional.regression.squared_error(*data, self.reduction)
 
