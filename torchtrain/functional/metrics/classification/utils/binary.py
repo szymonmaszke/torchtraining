0b2241deb7aa9331f7dec0976be62370c1c6945e
vyzyv <vyz@protonmail.com>
2020-08-13 20:15:31 +0200

    Document and add typing to classification

--- torchtrain/functional/metrics/classification/utils/binary.py
+++ torchtrain/functional/metrics/classification/utils/binary.py
@@ -1,4 +1,25 @@
-def check(output, target):
+"""Utilities used with most binary metrics."""
+
+import typing
+
+import torch
+
+
+def check(output: torch.Tensor, target: torch.Tensor) -> None:
+    """
+    Check `output` and `target` shape is equal.
+
+    ValueError is raised otherwise
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    """
+
     if output.shape != target.shape:
         raise ValueError(
             "Output and target has to be of the same shape! Got {} for output and {} for target".format(
@@ -7,5 +28,27 @@ def check(output, target):
         )
 
 
-def threshold(output, target, threshold):
+def threshold(
+    output: torch.Tensor, target: torch.Tensor, threshold: float
+) -> typing.Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Threshold output logits/probabilities at `thresholds`.
+
+    For logits, `threshold` should be set to `0`, while for `probabilities`
+    it should be `0.5`
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+    threshold : float
+        Value at which `output`will be thresholded
+
+    Returns
+    -------
+    typing.Tuple[torch.Tensor, torch.Tensor]:
+        Thresholded output and `target` being a boolean for faster computations.
+    """
     return output > threshold, target.bool()
--- torchtrain/functional/metrics/classification/utils/multiclass.py
+++ torchtrain/functional/metrics/classification/utils/multiclass.py
@@ -1,7 +1,23 @@
+import typing
+
 import torch
 
 
-def dimensions_number_check(output, target):
+def dimensions_number_check(output: torch.Tensor, target: torch.Tensor) -> None:
+    """
+    Validate output and target number of dimensions.
+
+    Output should have one dimension more than target as those are probabilities
+    or logits.
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    """
     if len(output.shape) - 1 != len(target.shape):
         raise ValueError(
             "Got {} number of dimensions for output and {} for target. "
@@ -11,8 +27,19 @@ def dimensions_number_check(output, target):
         )
 
 
-def shape_check(output, target):
-    if output.shape != target.shape[:-1]:
+def shape_check(output: torch.Tensor, target: torch.Tensor) -> None:
+    """
+    Validate output and target shapes (except last dimension).
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    """
+    if output.shape[:-1] != target.shape:
         raise ValueError(
             "Got {} shape for output and {} for target. "
             "Shape should be equal except for last dimension of output.".format(
@@ -21,22 +48,92 @@ def shape_check(output, target):
         )
 
 
-def check(output, target):
-    _dimensions_number_check(output, target)
-    _shape_check(output, target)
+def check(output: torch.Tensor, target: torch.Tensor) -> None:
+    """
+    Run all multiclass checks.
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    """
+    dimensions_number_check(output, target)
+    shape_check(output, target)
 
 
-def categorical(output, target):
+def categorical(
+    output: torch.Tensor, target: torch.Tensor
+) -> typing.Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Transform output and target to categorical.
+
+    Additionally, `target` will be transformed to `long` for faster comparison
+    (as it is already a categorical).
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    Returns
+    -------
+    typing.Tuple[torch.Tensor, torch.Tensor]:
+        Categorical `output` and `target`.
+
+    """
     return torch.argmax(output, dim=-1), target.long()
 
 
-def one_hot(output, target, num_classes):
-    categorical_output, categorical_target = _categorical(output, target)
+def one_hot(
+    output: torch.Tensor, target: torch.Tensor, num_classes: int = None
+) -> typing.Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Transform output and target to one hot encoding.
+
+    Both will be transformed to categorical beforehand
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    Returns
+    -------
+    typing.Tuple[torch.Tensor, torch.Tensor]:
+        One hot encoded `output` and `target`.
+
+    """
+    categorical_output, categorical_target = categorical(output, target)
     return (
         torch.nn.functional.one_hot(output, num_classes),
         torch.nn.functional.one_hot(target, num_classes),
     )
 
 
-def get_num_classes(output, target):
+def get_num_classes(output: torch.Tensor, target: torch.Tensor) -> int:
+    """
+    Get number of classes present in output & target.
+
+    Used only by `confusion_matrix` (for now...)
+
+    Parameters
+    ----------
+    output : torch.Tensor
+        Usually neural network output
+    target : torch.Tensor
+        Usually desired target
+
+    Returns
+    -------
+    int
+        Total number of classes (maximum from `output` and `target`)
+
+    """
     return max(output.shape[-1], torch.max(target).item())
