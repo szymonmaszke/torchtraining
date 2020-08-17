65a7a69c7ed3e080015f5400f5f2993123c9144f
vyzyv <vyz@protonmail.com>
2020-08-17 16:41:56 +0200

    Add dosctrings

--- torchtrain/quantization.py
+++ torchtrain/quantization.py
@@ -4,5 +4,15 @@ from ._base import Op
 
 
 class Dequantize(Op):
+    """Given a quantized Tensor, dequantize it and return an fp32 Tensor.
+
+    Arguments
+    ---------
+    data: torch.Tensor
+        Quantized `torch.Tensor` to dequantize.
+
+
+    """
+
     def forward(self, data):
         return data.dequantize()
