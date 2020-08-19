d6856d0696b59822a9b564f55d092033d5fbfe95
vyzyv <vyz@protonmail.com>
2020-08-19 19:24:07 +0200

    Add docs for mixup

--- torchtrain/functional/inputs.py
+++ torchtrain/functional/inputs.py
@@ -1,8 +1,8 @@
 """
-
-
 This module has no object oriented counterpart and should be used
-in a functional fashion when writing `forward` of `torchtrain.steps`.
+in a functional fashion inside `forward` of `torchtrain.steps`.
+
+See `examples` for specific user cases
 
 """
 
@@ -19,10 +19,37 @@ def mixup(
     See [mixup: Beyond Empirical Risk Minimization](https://arxiv.org/abs/1710.09412)
     for explanation of the method.
 
-    Users sho
+    Example::
+
+        class TrainStep(tt.steps.Train):
+            def forward(self, module, sample):
+                images, labels = sample
+                images, labels = tt.functional.inputs.mixup(images, labels)
+                # Calculate what you want below, say loss
+                ...
+                return loss
+
+
+        step = TrainStep(criterion, device)
+
+    **IMPORTANT:** Examples are modified in-place!
 
     Arguments
     ---------
+    inputs: torch.Tensor
+        `torch.Tensor` of any shape and numerical `dtype`.
+    labels: torch.Tensor
+        `torch.Tensor` of any shape and numerical `dtype`.
+    gamma: float
+        Level of mixing between inputs and labels. The smaller the value,
+        the more "concrete" examples are (e.g. for  `0.1` and `cat`, `dog` labels
+        it would be `0.9` cat and `0.1` dog).
+
+    Returns
+    -------
+    Tuple(torch.Tensor, torch.Tensor)
+        Inputs and labels after mixup (linear mix with `gamma` strength).
+
     """
     perm = torch.randperm(inputs.shape[0])
     perm_inputs = inputs[perm]
